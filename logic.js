9de7609a5a74dd46d67827a7e0995998ae86552b
31Pritika <pritikaagarwal0731@gmail.com>
2026-03-28 17:14:26 +0000

    everything done

--- logic.js
+++ logic.js
@@ -1,33 +1,82 @@
 function getComputerChoice() {
     let choice = ['rock', 'paper', 'scissors'];
-    let index = Math.floor(Math.random() * choice.length());
+    let index = Math.floor(Math.random() * choice.length);
     return choice[index];
 }
 
 function getHumanChoice() {
     let input = prompt("Enter rock/paper/scissors");
 
-    if(!input){
+    if (!input) {
         console.log("Game cancelled!!");
         return null;
     }
 
     let i = input.toLowerCase();
-    if(i != 'rock' && i != 'paper' && i != 'scissors'){
+
+    if (i != 'rock' && i != 'paper' && i != 'scissors') {
         console.log("invalid entry, only enter rock, paper or scissors!!");
         return null;
     }
-    
-    console.log(input);
-    return(input);
-    
+
+    return i;
 }
 
-function playRound() {
+function playRound(human, comp) {
+    if (human === comp) {
+        return `Both chose the same ${human}, therefore a tie`;
+    }
 
+    if (
+        (comp === 'paper' && human === 'rock') || 
+        (comp === 'scissors' && human === 'paper') ||
+        (comp === 'rock' && human === 'scissors')
+    ) {
+        return `human`;
+    } else {
+        return `comp`;
+    }
 }
 
-const human = getHumanChoice();
-const comp = getComputerChoice();
+let hscore = 0;
+let cscore = 0; 
 
-playRound(human, computer);
+for (let i = 0; i < 5; i++) {
+    const human = getHumanChoice();
+
+    if (human === null) {
+        console.log('skipping human choice...');
+        i--;
+        continue;
+    }
+
+    const comp = getComputerChoice();
+    const ans = playRound(human, comp);
+
+    console.log(`Round ${i + 1}`);
+    console.log("Human:", human);
+    console.log("Computer:", comp);
+    console.log(ans);
+
+    if (ans === `human`) {
+        hscore++;
+    } else if (ans === `comp`) {
+        cscore++;
+    } else {
+        console.log(`round is a tie`);
+    }
+
+    console.log("----------------------");
+}
+
+console.log("Final Score:");
+console.log("Human:", hscore);
+console.log("Computer:", cscore);
+
+if (hscore > cscore) {
+    console.log("🏆 Human wins the game!");
+} else if (cscore > hscore) {
+    console.log("💻 Computer wins the game!");
+} else {
+    console.log("🤝 The game is a tie!");
+}
